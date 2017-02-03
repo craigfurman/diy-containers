5c37450cc7e76dbf997bab0032052f5b8ee1bcac
Craig Furman <cfurman@pivotal.io>
2017-02-03 13:49:38 +0000

    optionally pin process to certain cpu cores

--- go/src/container-run/main.go
+++ go/src/container-run/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"syscall"
 )
 
@@ -20,13 +22,18 @@ func main() {
 
 func parent() {
 	rootFS := flag.String("rootFS", "", "rootFS")
+	coresToUse := flag.String("cores", "", "OPTIONAL: which cores to use? examples: '0', or '0-1'")
 	flag.Parse()
 	if *rootFS == "" {
 		fmt.Println("must set -rootFS")
 		os.Exit(1)
 	}
+	cores := fmt.Sprintf("0-%d", runtime.NumCPU()-1)
+	if *coresToUse != "" {
+		cores = *coresToUse
+	}
 
-	cmd := exec.Command("/proc/self/exe", append([]string{"child", *rootFS}, flag.Args()...)...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child", *rootFS, cores}, flag.Args()...)...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWNS,
 	}
@@ -46,6 +53,8 @@ func parent() {
 
 func child() {
 	rootFS := os.Args[2]
+	cores := os.Args[3]
+
 	must(syscall.Mount("", "/", "", syscall.MS_PRIVATE, ""))
 	must(os.MkdirAll(filepath.Join(rootFS, "oldrootfs"), 0700))
 	must(syscall.Mount(rootFS, rootFS, "", syscall.MS_BIND, ""))
@@ -55,7 +64,15 @@ func child() {
 	must(syscall.Mount("", "/proc", "proc", 0, ""))
 	must(syscall.Unmount("/oldrootfs", syscall.MNT_DETACH))
 
-	cmd := exec.Command(os.Args[3], os.Args[4:]...)
+	must(syscall.Mount("cgroup_root", "/sys/fs/cgroup", "tmpfs", 0, ""))
+	must(os.MkdirAll("/sys/fs/cgroup/cpuset", 0700))
+	must(syscall.Mount("cpuset", "/sys/fs/cgroup/cpuset", "cgroup", 0, "cpuset"))
+	must(os.MkdirAll("/sys/fs/cgroup/cpuset/container", 0700))
+	must(ioutil.WriteFile("/sys/fs/cgroup/cpuset/container/cpuset.mems", []byte("0"), 0600))
+	must(ioutil.WriteFile("/sys/fs/cgroup/cpuset/container/cpuset.cpus", []byte(cores), 0600))
+	must(ioutil.WriteFile("/sys/fs/cgroup/cpuset/container/tasks", []byte(fmt.Sprintf("%d", os.Getpid())), 0600))
+
+	cmd := exec.Command(os.Args[4], os.Args[5:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
